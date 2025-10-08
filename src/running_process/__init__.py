8852691ef4540d82249f669985e5ba584ba7e0d5
zackees <z@zackees.com>
2025-10-08 04:18:37 -0700

    feat(project): Bump version to 1.0.5

--- src/running_process/__init__.py
+++ src/running_process/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__ = "1.0.1"
+__version__ = "1.0.5"
 
 from running_process.output_formatter import OutputFormatter, TimeDeltaFormatter
 from running_process.process_utils import get_process_tree_info, kill_process_tree
